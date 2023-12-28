3b2a268e689f86ece8c40a4f9eefed37f5e84866
Jenni <jennifer.1707@hotmail.fr>
2023-12-28 17:18:04 +0100

    version 1.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,49 @@
+// La galerie
+let Works = [];
+
 const gallery = document.querySelector(".gallery");
-const pieces = await fetch("http://localhost:5678/api/works");
+const getworks = () => {
+  fetch("http://localhost:5678/api/works/")
+    .then((res) => res.json())
+    .then((data) => {
+      Works = data;
+      displayworks();
+    });
+};
+
+const displayworks = () => {
+  gallery.innerHTML = "";
+
+  Works.forEach((element) => {
+    gallery.innerHTML += `<figure>
+        <img src=${element.imageUrl} alt="Abajour Tahina">
+            <figcaption>${element.title}</figcaption>
+    </figure>`;
+  });
+};
+
+//Les filtres
+let categories = [];
+const button = document.querySelector(".categories");
+const createinputElement = () => {
+  for (let i = 0; i < categories.length; i++) {
+    const input = document.createElement("input");
+    button.appendChild(input);
+    input.type = "submit";
+    input.value = categories[i].name;
+  }
+};
+
+const getcategories = () => {
+  fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      categories = data;
+      createinputElement();
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  getworks();
+  getcategories();
+});
