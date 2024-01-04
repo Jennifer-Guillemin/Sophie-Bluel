1998a24d55e72238c2e2bc43dff4c6af50177f61
Jenni <jennifer.1707@hotmail.fr>
2024-01-04 18:59:44 +0100

    version 1.4

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,17 +26,6 @@ getworks();
 
 //Les filtres
 let categories = [];
-const button = document.querySelector(".categories");
-const createinputElement = () => {
-  for (let i = 0; i < categories.length; i++) {
-    const input = document.createElement("input");
-    button.appendChild(input);
-    input.type = "submit";
-    input.value = categories[i].name;
-  }
-};
-
-const Filtres = document.querySelectorAll("filtres");
 
 const getcategories = () => {
   fetch("http://localhost:5678/api/categories")
@@ -47,9 +36,69 @@ const getcategories = () => {
     });
 };
 
+const button = document.querySelector(".categories");
+const createinputElement = () => {
+  for (let i = 0; i < categories.length; i++) {
+    const input = document.createElement("input");
+    button.appendChild(input);
+    input.type = "submit";
+    input.value = categories[i].name;
+
+    input.addEventListener("click", () => {
+      handleInputClick(categories[i].id, categories[i].name);
+      const allInputs = document.querySelectorAll(".categories input");
+
+      allInputs.forEach((otherInput) => {
+        otherInput.classList.remove("selected");
+      });
+
+      input.classList.add("selected");
+      const categorieId = categories[i].id;
+      displayWorksByCategorie(categorieId);
+    });
+  }
+};
+
+const filtres = document.getElementById("filtres");
+
+filtres.addEventListener("click", (input) => {
+  displayworks();
+  let i = 0;
+  input.className = "";
+  console.log("Tous", [i]);
+
+  const allInputs = document.querySelectorAll(".categories input");
+  allInputs.forEach((input) => {
+    input.classList.remove("selected");
+  });
+
+  filtres.classList.add("selected");
+});
+
 getcategories();
 
+const handleInputClick = (categorieId, categorieName) => {
+  console.log(" id:", categorieId, ", name :", categorieName);
+};
+
+const displayWorksByCategorie = (categorieId) => {
+  const filteredWorks = Works.filter((work) => work.categoryId === categorieId);
+  displayFilteredWorks(filteredWorks);
+};
+
+const displayFilteredWorks = (filteredWorks) => {
+  gallery.innerHTML = "";
+
+  filteredWorks.forEach((element) => {
+    gallery.innerHTML += `<figure>
+        <img src=${element.imageUrl} alt="${element.title}">
+            <figcaption>${element.title}</figcaption>
+    </figure>`;
+  });
+};
+
 // Formulaire de connexion
+
 function validateForm() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
